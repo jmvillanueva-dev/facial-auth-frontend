f38a1a73ce851f32e6f75d146ca4f6d91723e2f3
jmvillanueva-dev <jhonny.villanueva@epn.edu.ec>
2025-07-22 19:04:08 -0500

    feat: Update Vercel configuration and enhance Vite build settings

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/",
   build: {
     outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`,
+      },
+    },
   },
   assetsInclude: ["**/*.html"],
 });
